8015fb3119c156763b0372f514d91f089ae4d6eb
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-04-20 22:29:40 +0200

    Collect meanings of the phrases

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import json
 import sys
+import re
 
 import requests
 from bs4 import BeautifulSoup
@@ -17,6 +19,27 @@ def fetch_soup(url):
 base_url = 'https://pl.wiktionary.org'
 index_url = base_url + '/wiki/Indeks:Polski_-_Zwi%C4%85zki_frazeologiczne'
 
+results = []
 
 for link in fetch_soup(index_url).select('.mw-parser-output ul li a'):
-    print(link.text)
+    if link.get('href').endswith('(strona nie istnieje)'):
+        continue
+
+    meanings = []
+    meanings_soup = fetch_soup(base_url + link.get('href'))
+
+    meaning_section = meanings_soup.select(
+        '#mw-content-text .mw-parser-output dl')
+
+    try:
+        for meaning in meaning_section[2].find_all('dd'):
+            meaning_text = re.sub(r'\(\d+\.\d+\) ', '', meaning.get_text())
+            meanings.append(meaning_text)
+    except IndexError:
+        print(f'Warning - {link.text} has no meanings section')
+        continue
+
+    results.append({
+        'phrase': link.get_text(),
+        'meanings': meanings
+    })
