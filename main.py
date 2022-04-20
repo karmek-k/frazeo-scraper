18684e5f35318b6478c89873356f6954762678d2
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-04-20 21:48:10 +0200

    Fetch phrases from the index page

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+import re
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def fetch_soup(url):
+    res = requests.get(index_url)
+
+    if res.status_code != 200:
+        print(f'Could not download the page, code: {res.status_code}')
+        sys.exit(1)
+
+    return BeautifulSoup(res.text, 'lxml')
+
+
+base_url = 'https://pl.wiktionary.org'
+index_url = base_url + '/wiki/Indeks:Polski_-_Zwi%C4%85zki_frazeologiczne'
+
+
+for link in fetch_soup('index_url').select('.mw-parser-output ul li a'):
+    print(link.text)
